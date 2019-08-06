e27fc4623ebe76a168de6eb3bbae72122e96cf96
Albert Dang <adang018@ucr.edu>
2019-08-05 19:24:13 -0700

    Started home page

---
+++ components/HomePage.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { AppRegistry, Text, View, StyleSheet } from 'react-native';
+
+export default class HomePage extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    }
+}); 
