960a981d9bf37bfeacb6d9a6f37689c3ade9482b
Albert Dang <adang018@ucr.edu>
2019-08-13 10:24:37 -0700

    Made home page

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import HomePage from './components/HomePage';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <HomePage />
     </View>
   );
 }
--- components/HomePage.js
+++ components/HomePage.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
-import { AppRegistry, Text, View, StyleSheet } from 'react-native';
+import { Text, View, ImageBackground, TouchableOpacity, StyleSheet } from 'react-native';
 
 export default class HomePage extends Component {
     render() {
         return (
             <View style={styles.container}>
+                <ImageBackground source={require("../resources/homepage/stc.jpg")} style={styles.background}>
+                    <View style={styles.header}>
+                        <Text style={styles.headerText}>CNH Circle K!</Text>
+                    </View>
+                    <View style={styles.nav}>
+                        <View style={styles.navRow}>
+                            <TouchableOpacity style={styles.navButton}>
+                                <Text style={styles.navText}>Venue Map</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity style={styles.navButton}>
+                                <Text style={styles.navText}>Schedule</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity style={styles.navButton}>
+                                <Text style={styles.navText}>Welcomes</Text>
+                            </TouchableOpacity>
+                        </View>
+                        <View style={styles.navRow}>
+                            <TouchableOpacity style={styles.navButton}>
+                                <Text style={styles.navText}>General Info</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity style={styles.navButton}>
+                                <Text style={styles.navText}>Documents</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity style={styles.navButton}>
+                                <Text style={styles.navText}>CNH People</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                </ImageBackground>
             </View>
         );
     }
@@ -12,6 +41,49 @@ export default class HomePage extends Component {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1
+        flex: 1,
+    },
+    background: {
+        width: "100%",
+        height: "100%"
+    },
+    header: {
+        marginTop: 60,
+        padding: 20,
+        backgroundColor: "rgba(0, 0, 0, 0.5)",
+        borderColor: "rgba(255, 30, 30, 0.1)",
+        borderTopWidth: 2,
+        borderBottomWidth: 2,
+    },
+    headerText: {
+        fontSize: 40,
+        fontWeight: "bold",
+        color: "white",
+        textShadowRadius: 100,
+        textAlign: "center"
+    },
+    nav: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    navRow: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center"
+    },  
+    navButton: {
+        height: 100,
+        width: 100,
+        margin: 5,
+        backgroundColor: "rgba(0, 0, 0, 0.8)",
+        borderRadius: 50,
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    navText: {
+        fontSize: 14,
+        fontWeight: "bold",
+        color: "white"
     }
 }); 
