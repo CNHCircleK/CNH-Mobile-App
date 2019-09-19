bd95ed09a7524b2dc312aa0d8c65b22997f1d7fb
Unknown <tdiaz@hmc.edu>
2019-09-18 23:15:43 -0700

    team captains page

--- pages/TemplateInfoPage.js
+++ pages/TemplateInfoPage.js
@@ -10,27 +10,34 @@ export default class TemplateInfoPage extends Component {
   constructor(props){
     super(props);
     this.state = {
-      title: Res.strings.templateinfo.title,
-      header: Res.strings.templateinfo.header,
-      image: require('../resources/images/HomePage/FTC2019_LOGO.png'),
-      body: Res.strings.templateinfo.body
+      title: props.title,  //Res.strings.templateinfo.title,
+      header: props.header,  //Res.strings.templateinfo.header,
+      image:  props.image, //require('../resources/images/HomePage/FTC2019_LOGO.png'),
+      body :props.body  //Res.strings.templateinfo.body
     };
   }
     render() {
         return (
             <ScrollView>
                 <View style={styles.container}>
-                <InfoTitle>
-                {this.state.title}{'\n'}
-                </InfoTitle>
+                {this.state.title !== undefined &&
+                  <InfoTitle>
+                  {this.state.title}{'\n'}
+                  </InfoTitle>
+                }
+                {this.state.image !== undefined &&
                 <Image style={{width: 250, height: 250}}
-
+                resizeMode="contain"
                 source={this.state.image}/>
-
-                <InfoHeader>{this.state.header}{'\n'}</InfoHeader>
+                }
+                {this.state.header !== undefined &&
+                <InfoHeader>{'\n'}{this.state.header}{'\n'}</InfoHeader>
+                }
+                {this.state.body !== undefined &&
                 <InfoParagraph >
                 {this.state.body}
                 </InfoParagraph>
+                }
                 </View>
             </ScrollView>
         );
--- resources/strings.js
+++ resources/strings.js
@@ -46,6 +46,27 @@ of the spectator on a hillside, the sable cloud beneath was dished out,\
 and the car seemed to float in the middle of an immense dark sphere,\
 whose upper half was strewn with silver. Looking down into the dark gulf below,\
 I could see a ruddy light streaming through a rift in the clouds.`
+  },
+  teamCaptain: {
+    title: "Team Captain",
+    image: require("./images/LeadershipOpportunities/teamcaptains.png"),
+    header: "Due: 9/27",
+    body: `Greetings! We are Jason & Thuy and we are seeking out Team Captains to unveil the truth \
+of FTC alongside your team members! Being a Team Captain allows you to pick up leadership skills\
+and truly influence the experience of FTC on those around you. There's nothing more special than\
+bonding through competitive games and making fun jokes with all sorts of people across the district!
+
+Team Captain Duties:
+● Communicating with Team Activities Co-Chairs, Jason & Thuy, before and during Fall\
+Training Conference 2019.
+● Attending Team Captain Meetings prior to Fall Training Conference and becoming\
+comfortable with the rules and regulations of all activities.
+● Being an authoritative leader while also creating a positive environment for your team\
+members.
+● Making sure everyone is participating and staying engaged.
+● Leading roughly 10 to 20 members throughout games and activities.
+● Creating an open and friendly space for team members.
+● Having fun! c:`
   }
 }
 
