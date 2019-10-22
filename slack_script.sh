ca6391712b5895b241e5b8ec5e8302d5a4432405
Unknown <tdiaz@hmc.edu>
2019-10-22 15:29:14 -0700

    Added two branches for slack bash script for dev and master

--- slack_script.sh
+++ slack_script.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
-if [ $# -ne 1 ]; then
-    echo $0: usage: myscript name
+if [ $# -ne 2 ]; then
+    echo $0: usage: slack_script.sh webhookurl [dev, master]
     exit 1
 fi
 
 url=$1
+branch=$2
 
-curl -X POST -H 'Content-type: application/json' --data '{"text":"A new project has been published to https://exp.host/@cnhtechsoftware/"}' $url
+if [ $branch = "dev" ]; then
+    curl -X POST -H 'Content-type: application/json' --data '{"text":"A new DEVELOPMENT
+    build has been published to https://exp.host/@cnhtechsoftware/solstice?release-channel=development"}' $url
+
+elif [ $branch = "master" ]; then
+    curl -X POST -H 'Content-type: application/json' --data '{"text":"A new PRODUCTION
+    build has been published to https://exp.host/@cnhtechsoftware/solstice"}' $url
+else
+    echo "Input must either dev or master after webhook url"
+    echo $branch
+fi
