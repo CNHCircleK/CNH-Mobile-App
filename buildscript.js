fc7f5e3d9289961b4a5639b6ab66bebacc2ea460
Unknown <tdiaz@hmc.edu>
2019-10-25 15:05:38 -0700

    Move gitscript inside buildscript

--- buildscript.js
+++ buildscript.js
@@ -16,4 +16,12 @@ appData.expo.version = version;
 jsonfile.writeFileSync(buildNumFile, buildNumData, { spaces: 2, EOL: '\r\n' });
 jsonfile.writeFileSync(appFile, appData, { spaces: 2, EOL: '\r\n' });
 
-shell.exec('gitscript.sh ' + version);
+shell.exec(`git config --global user.email "cnhtech.software@gmail.com"`);
+shell.exec(`git config --global user.name "CNH Software Team"`);
+
+//shell.exec("git checkout -b master_ci_pipeline");
+shell.exec("git add .");
+shell.exec(`git commit --message "Release version: "` + appData.expo.version);
+
+shell.exec(`git remote add master_ci_pipeline https://${GH_TOKEN}@github.com/CNHCircleK/CNH-Mobile-App.git > /dev/null 2>&1`);
+shell.exec("git push --quiet --set-upstream master_ci_pipeline master_ci_pipeline");
