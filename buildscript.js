7ffa938e281a00f6ebc3d2c0a1cce85d5e425d26
Terrence James Diaz <tdiaz@hmc.edu>
2019-10-25 17:13:46 -0700

    Ci/CD Pipeline that commits/pushes a new build number and publishes to production/development (#26)
    * Update app.json
    
    * Fixed iOS sticky header bug (#20) (#21)
    
    * add a script to increment the ota build number
    
    * Run git script to commit build numbers and push/tag to master
    
    * fixed ci build crash
    
    * Fixed gitscript not executing correctly in buildscript
    
    * Move gitscript inside buildscript
    
    * pass in gh token from travis instead of inside buildscript
    
    * Try to fix git push not working
    
    * prevent build from running on builduser
    
    * Change how version number is incremented to keep separated from OTA
    
    * remove references to master_ci_pipeline
    
    * Delete gitscript.sh

---
+++ buildscript.js
@@ -0,0 +1,32 @@
+const shell = require('shelljs');
+const jsonfile = require('jsonfile');
+const buildNumFile = 'otaBuildNum.json';
+const appFile = 'app.json';
+
+const buildNumData = jsonfile.readFileSync(buildNumFile);
+buildNumData.otaBuildNum += 1;
+
+const appData = jsonfile.readFileSync(appFile);
+let version = appData.expo.version;
+let patchNum = version.charAt(version.length-1);
+let patchNumInt = parseInt(patchNum, 10);
+patchNumInt += 1;
+let newVersion = version.slice(0, -2);
+newVersion = newVersion + "." + patchNumInt.toString();
+
+appData.expo.version = newVersion;
+
+jsonfile.writeFileSync(buildNumFile, buildNumData, { spaces: 2, EOL: '\r\n' });
+jsonfile.writeFileSync(appFile, appData, { spaces: 2, EOL: '\r\n' });
+
+var args = process.argv.slice(2);
+shell.exec(`git config --global user.email "cnhtech.software@gmail.com"`);
+shell.exec(`git config --global user.name "CNH Software Team"`);
+
+shell.exec("git checkout master");
+shell.exec("git add .");
+shell.exec(`git commit --message "[Auto-generated] Release version: ${appData.expo.version}; otaBuildNum: ${buildNumData.otaBuildNum}"`);
+
+shell.exec("git remote rm origin");
+shell.exec(`git remote add origin https://cnhtech-software:${args}@github.com/CNHCircleK/CNH-Mobile-App.git > /dev/null 2>&1`);
+shell.exec("git push origin master");
