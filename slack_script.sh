efa6f6ae986f30506bd4da18b68c8d69242aadcd
Unknown <tdiaz@hmc.edu>
2019-10-28 15:54:53 -0700

    Fixed last commit message in webhook

--- slack_script.sh
+++ slack_script.sh
@@ -9,10 +9,10 @@ branch=$2
 commit=$3
 
 if [ $branch = "dev" ]; then
-    curl -X POST -H 'Content-type: application/json' --data '{"text":"A new DEVELOPMENT build has been published to https://exp.host/@cnhtechsoftware/solstice?release-channel=development"}. Last commit: ${commit}' $url
+    curl -X POST -H 'Content-type: application/json' --data '{"text":"A new DEVELOPMENT build has been published to https://exp.host/@cnhtechsoftware/solstice?release-channel=development". Last commit: ${commit}}.' $url
 
 elif [ $branch = "master" ]; then
-    curl -X POST -H 'Content-type: application/json' --data '{"text":"A new PRODUCTION build has been published to https://exp.host/@cnhtechsoftware/solstice"}. Last commit: ${commit}' $url
+    curl -X POST -H 'Content-type: application/json' --data '{"text":"A new PRODUCTION build has been published to https://exp.host/@cnhtechsoftware/solstice". Last commit: ${commit}}.' $url
 else
     echo "Input must either dev or master after webhook url"
     echo $branch
