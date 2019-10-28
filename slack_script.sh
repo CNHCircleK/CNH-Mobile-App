8dbb0125ec50bc1801479e012e5133818c4b7919
Terrence James Diaz <tdiaz@hmc.edu>
2019-10-28 15:30:48 -0700

    Trigger build on PRs, tag master after pushing commit, and display la… (#31)
    * Trigger build on PRs, tag master after pushing commit, and display last commit in expo publish
    
    * do not trigger peepo devops slack notifications on PRs

--- slack_script.sh
+++ slack_script.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
-if [ $# -ne 2 ]; then
-    echo $0: usage: slack_script.sh webhookurl [dev, master]
+if [ $# -ne 3 ]; then
+    echo $0: usage: slack_script.sh webhookurl [dev, master] commit_msg
     exit 1
 fi
 
 url=$1
 branch=$2
+commit=$3
 
 if [ $branch = "dev" ]; then
-    curl -X POST -H 'Content-type: application/json' --data '{"text":"A new DEVELOPMENT build has been published to https://exp.host/@cnhtechsoftware/solstice?release-channel=development"}' $url
+    curl -X POST -H 'Content-type: application/json' --data '{"text":"A new DEVELOPMENT build has been published to https://exp.host/@cnhtechsoftware/solstice?release-channel=development"}. Last commit: ${commit}' $url
 
 elif [ $branch = "master" ]; then
-    curl -X POST -H 'Content-type: application/json' --data '{"text":"A new PRODUCTION build has been published to https://exp.host/@cnhtechsoftware/solstice"}' $url
+    curl -X POST -H 'Content-type: application/json' --data '{"text":"A new PRODUCTION build has been published to https://exp.host/@cnhtechsoftware/solstice"}. Last commit: ${commit}' $url
 else
     echo "Input must either dev or master after webhook url"
     echo $branch
