eec53611a84076b076bcb2538fa54bf8aeba6fc5
“Allison <im.allison.chu@gmail.com>
2019-10-29 22:30:43 -0700

    fixed center zoom

--- pages/MapPage.js
+++ pages/MapPage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, Image, ScrollView, Dimensions, ImageBackground, Text, SectionList, FlatList, TouchableOpacity, TouchableWithoutFeedback, Linking, Modal } from 'react-native';
+import { View, StyleSheet, Image, ScrollView, SafeAreaView, Dimensions, ImageBackground, Text, SectionList, FlatList, TouchableOpacity, TouchableWithoutFeedback, Linking, Modal } from 'react-native';
 import TemplateInfoPage from '@components/TemplateInfoPage';
 import ImageZoom from 'react-native-image-pan-zoom';
 import Res from '@resources';
@@ -7,13 +7,21 @@ import Res from '@resources';
 export default class MapPage extends Component {
     render() {
         return (
-            <ScrollView minimumZoomScale={1} maximumZoomScale={5} contentContainerStyle={{height: 1000}}>
-    <ScrollView minimumZoomScale={1} maximumZoomScale={5} horizontal contentContainerStyle={{width: 1000}}>
+            <ScrollView style={{height: 1200}}>
+    <ScrollView horizontal style={{width: 1200}}>
+    <ImageZoom cropWidth={Dimensions.get('window').width}
+                       cropHeight={Dimensions.get('window').height}
+                       imageWidth={1550}
+                       imageHeight={1300}
+                       enableCenterFocus = {false}>
     <Image 
-            style={{width: 1000, height: 800}}
+            style={{width: 1500, height: 1200}}
             source={require('../resources/images/HomePage/map.png')} />
+            </ImageZoom>
     </ScrollView>
 </ScrollView>
+
+
         );
     }
 }
