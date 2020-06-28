3d9043134058da69702218849e294236735ec9d7
Terrence <terrencejamesdiaz@gmail.com>
2020-06-28 12:30:41 -0700

    format discord expo notification

--- slack_script.sh
+++ slack_script.sh
@@ -1,20 +1,39 @@
 #!/bin/bash
 # JSON payload is currently formatted for Discord webhooks despite filename
-if [ $# -ne 3 ]; then
-    echo $0: usage: slack_script.sh webhookurl [dev, master] commit_msg
+if [ $# -ne 2 ]; then
+    echo $0: usage: slack_script.sh webhookurl [dev, master]
     exit 1
 fi
 
 url=$1
 branch=$2
-commit=$3
 
 if [ $branch = "dev" ]; then
-    curl -X POST -H 'Content-type: application/json' --data "{'content':'A new DEVELOPMENT build has been published to https://exp.host/@cnhtechsoftware/solstice?release-channel=development. Last commit: ${commit}'}" $url
-
+  content="A new DEVELOPMENT build has been published."
+  expo_url="https://exp.host/@cnhtechsoftware/solstice?release-channel=development"
 elif [ $branch = "master" ]; then
-    curl -X POST -H 'Content-type: application/json' --data "{'content':'A new PRODUCTION build has been published to https://exp.host/@cnhtechsoftware/solstice. Last commit: ${commit}'}" $url
+  content="A new PRODUCTION build has been published."
+  expo_url="https://exp.host/@cnhtechsoftware/solstice"
 else
     echo "Input must either dev or master after webhook url"
     echo $branch
+    exit 1
 fi
+
+color=1942002
+
+WEBHOOK_DATA='{
+  "embeds": [ {
+    "color": '$color',
+    "author": {
+      "name": "'"$content"'",
+      "url": "'"$expo_url"'"
+    },
+    "title": "'"$COMMIT_SUBJECT"'",
+    "url": "'"$expo_url"'",
+    "description": "Open up the app in Expo for testing and usage.",
+  } ]
+}'
+
+
+curl -X POST -H 'Content-type: application/json' --data "$WEBHOOK_DATA" "$url"
