a8c3d2d243aa9469ba52c91131ed29ace43a3315
Terrence James Diaz <tdiaz@hmc.edu>
2020-06-28 11:45:09 -0700

    Change webhook script from Slack to Discord (#40)
    * Changed webhook payload from Slack to Discord
    
    * added travis-ci script for discord webhooks
    
    * removed avatar url
    
    Co-authored-by: Terrence <terrencejamesdiaz@gmail.com>

--- slack_script.sh
+++ slack_script.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# JSON payload is currently formatted for Discord webhooks despite filename
 if [ $# -ne 3 ]; then
     echo $0: usage: slack_script.sh webhookurl [dev, master] commit_msg
     exit 1
@@ -9,10 +10,10 @@ branch=$2
 commit=$3
 
 if [ $branch = "dev" ]; then
-    curl -X POST -H 'Content-type: application/json' --data "{'text':'A new DEVELOPMENT build has been published to https://exp.host/@cnhtechsoftware/solstice?release-channel=development. Last commit: ${commit}'}" $url
+    curl -X POST -H 'Content-type: application/json' --data "{'content':'A new DEVELOPMENT build has been published to https://exp.host/@cnhtechsoftware/solstice?release-channel=development. Last commit: ${commit}'}" $url
 
 elif [ $branch = "master" ]; then
-    curl -X POST -H 'Content-type: application/json' --data "{'text':'A new PRODUCTION build has been published to https://exp.host/@cnhtechsoftware/solstice. Last commit: ${commit}'}" $url
+    curl -X POST -H 'Content-type: application/json' --data "{'content':'A new PRODUCTION build has been published to https://exp.host/@cnhtechsoftware/solstice. Last commit: ${commit}'}" $url
 else
     echo "Input must either dev or master after webhook url"
     echo $branch
