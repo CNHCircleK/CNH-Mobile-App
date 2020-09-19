ffd6e0454f28a9e8543a16d5f64a88bf6246f07d
Vincent H <hoangvincent69@gmail.com>
2020-09-18 19:40:09 -0700

    About Us page added.

--- App.js
+++ App.js
@@ -1,23 +1,25 @@
-import React, {Component} from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
-import HomePage from './pages/HomePage';
-import DistrictLeadershipPage from './pages/DistrictLeadershipPage';
+import React, { Component } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import HomePage from "./pages/HomePage";
+import DistrictLeadershipPage from "./pages/DistrictLeadershipPage";
+import AboutPage from "./pages/AboutPage";
 
 const Stack = createStackNavigator();
 
 export default class App extends Component {
-    
-    render() {
-        return (
-            <NavigationContainer>
-                <Stack.Navigator
-                    headerMode='none'
-                >
-                    <Stack.Screen name="Home" component={HomePage} />
-                    <Stack.Screen name="District Leadership" component={DistrictLeadershipPage} />
-                </Stack.Navigator>
-            </NavigationContainer>
-        )
-    }
+  render() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator headerMode="none">
+          <Stack.Screen name="Home" component={HomePage} />
+          <Stack.Screen
+            name="District Leadership"
+            component={DistrictLeadershipPage}
+          />
+          <Stack.Screen name="About Us" component={AboutPage} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
 }
