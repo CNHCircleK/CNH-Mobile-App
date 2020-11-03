6c4b5880966a061da22651d7d0ef055a12e31195
AlbertDang46 <32824146+AlbertDang46@users.noreply.github.com>
2020-11-03 01:38:19 -0800

    Ftconly (#60)
    * Removed main part of app
    
    * removed more references to main app images

--- App.js
+++ App.js
@@ -2,12 +2,12 @@ import React, { Component } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import HomePage from "./pages/HomePage";
-import DistrictLeadershipPage from "./pages/DistrictLeadershipPage";
-import AboutPage from "./pages/AboutPage";
-import DFIPage from "./pages/DFIPage";
-import MRPPage from "./pages/MRPPage";
-import ResourcesPage from "./pages/ResourcesPage";
+// import HomePage from "./pages/HomePage";
+// import DistrictLeadershipPage from "./pages/DistrictLeadershipPage";
+// import AboutPage from "./pages/AboutPage";
+// import DFIPage from "./pages/DFIPage";
+// import MRPPage from "./pages/MRPPage";
+// import ResourcesPage from "./pages/ResourcesPage";
 import FTCSchedulePage from "./pages/FTC/FTCSchedulePage";
 import FTCScheduleDetailsPage from "./pages/FTC/FTCScheduleDetailsPage";
 import FTCTeamActivitiesPage from "./pages/FTC/FTCTeamActivitiesPage";
@@ -148,12 +148,12 @@ export default class App extends Component {
                 <NavigationContainer>
                     <Stack.Navigator headerMode="none">
                         <Stack.Screen name="FTC Tabs" component={FTCTabScreen} />
-                        <Stack.Screen name="Home" component={HomePage} />
+                        {/* <Stack.Screen name="Home" component={HomePage} />
                         <Stack.Screen name="About Us" component={AboutPage} />
                         <Stack.Screen name="Fundraising Initiatives" component={DFIPage} />
                         <Stack.Screen name="MRP" component={MRPPage} />
                         <Stack.Screen name="District Leadership" component={DistrictLeadershipPage} />
-                        <Stack.Screen name="Resources" component={ResourcesPage} />
+                        <Stack.Screen name="Resources" component={ResourcesPage} /> */}
                     </Stack.Navigator>
                 </NavigationContainer>
             );
