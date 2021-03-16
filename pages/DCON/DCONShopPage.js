eeff06481fdcabea51b6d68f2ed002d5cb5ae9ac
branraisin <50192935+branraisin@users.noreply.github.com>
2021-03-15 18:05:10 -0700

    Shop changes
    Added image and buttons for 'order form' and 'purchase items'

--- pages/DCON/DCONShopPage.js
+++ pages/DCON/DCONShopPage.js
@@ -17,6 +17,7 @@ export default class DCONShopPage extends Component {
                     </View>
                     
                     <View style={styles.itemcontainer}>
+
                         <View style={styles.introItem}>
                             <Text style={styles.introText}>Welcome to the District Convention 2021: </Text>
                             <Text style={styles.introText}>Forecasting Meaningful Service merchandise shop!{"\n"}</Text>
@@ -24,11 +25,24 @@ export default class DCONShopPage extends Component {
                                                         to fill out its order form avaliable in 'Resources' 
                                                         and submit a payment via Paypal. Happy shopping!</Text>
                         </View>
+                        <Image
+                            style={styles.introImage}
+                            source={require('../../resources/dcon2020/images/Harley-01.jpg')}
+                        />
                     </View>
                     
-                    <View style={styles.itemcontainer}>
-                        <TouchableOpacity style={styles.shopItem} onPress={ () => navigate("Shoutout") }>
-                            <Text style={styles.shopText}>DCON 2021 Souvenir Package</Text>
+                    <View style={styles.shopContainer}>
+                        <Image
+                            style={styles.shopImage}
+                            source={require('../../resources/dcon2020/images/DCONpackage.jpg')}
+                        />
+                        <Text style={styles.shopText}>DCON 2021 Souvenir Package</Text>
+                        <Text style={styles.shopPrice}>20$</Text>
+                        <TouchableOpacity style={styles.orderButton} onPress={ () => navigate("Shoutout") }>
+                            <Text style={styles.shopButtonText}>ORDER FORM</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.purchaseButton} onPress={ () => navigate("Shoutout") }>
+                            <Text style={styles.shopButtonText}>PURCHASE ITEM</Text>
                         </TouchableOpacity>
                     </View>
 
@@ -53,34 +67,23 @@ const styles = StyleSheet.create({
     },
     itemcontainer: {
     },
-    shopItem: {
+    introImage: {
         flex: 1,
-        height: 350,
-        marginHorizontal: 30,
-        marginBottom: 50,
-        backgroundColor: Res.FTCColors.TeaGreen,
-        borderRadius: 10,
-        justifyContent: 'center',
-        alignItems: 'center',
-        shadowColor: "#000000",
-        shadowOffset: {
-            width: 0,
-            height: 4,
-        },
-        shadowOpacity: 0.30,
-        shadowRadius: 4.65,
-        elevation: 8,
+        height: "100%", 
+        position: 'absolute',
+        resizeMode: "contain",
     },
     introItem: {
-        flex: 1,
-        height: 175,
-        marginHorizontal: 30,
-        marginVertical: 30,
         backgroundColor: 'white',
         borderRadius: 10,
+        elevation: 8,
+        flex: 1,
+        height: 175,
+        justifyContent: 'center',
+        marginHorizontal: 30,
+        marginVertical: 30,
         paddingLeft: 60,
         paddingRight: 10,
-        justifyContent: 'center',
         shadowColor: "#000000",
         shadowOffset: {
             width: 0,
@@ -88,38 +91,96 @@ const styles = StyleSheet.create({
         },
         shadowOpacity: 0.30,
         shadowRadius: 4.65,
-        elevation: 8,
-    },
-    shopText: {
-        fontSize: 50,
-        textAlign: 'center',
-        fontFamily: "Coolvetica",
     },
     introText: {
+        fontFamily: "Arial",
         fontSize: 14,
         textAlign: 'left',
-        fontFamily: "Coolvetica",
+    },
+    shopContainer : {
+        backgroundColor: 'white',
+        borderRadius: 10,
+        elevation: 8,
+        flexDirection: 'row',
+        height: 450,
+        marginHorizontal: 30,
+        marginBottom: 50,
+        position: 'relative',
+        shadowColor: "#000000",
+        shadowOffset: {
+            width: 0,
+            height: 4,
+        },
+        shadowOpacity: 0.30,
+        shadowRadius: 4.65,
+    },
+    orderButton: {
+        alignItems: 'center',
+        position: 'absolute',
+        width: "40%",
+        height: 30,
+        backgroundColor: 'white',
+        borderWidth: 2,
+        borderColor: 'yellow',
+        justifyContent: 'center',
+        bottom: 20,
+        left: 20,
+    },
+    purchaseButton: {
+        alignItems: 'center',
+        position: 'absolute',
+        width: "40%",
+        height: 30,
+        backgroundColor: '#FFEFA2',
+        justifyContent: 'center',
+        bottom: 20,
+        right: 20,
+    },
+    shopImage: {
+        borderTopLeftRadius: 10,
+        borderTopRightRadius: 10,
+        flex: 1,
+        height: "75%",
+        width: undefined,
+    },
+    shopText: {
+        position: 'absolute',
+        bottom: 65,
+        left: 20,
+        fontSize: 18,
+        fontFamily: "Arial",
+    },
+    shopPrice: {
+        position: 'absolute',
+        bottom: 65,
+        right: 20,
+        fontSize: 18,
+        fontFamily: "Arial",
+    },
+    shopButtonText: {
+        fontSize: 10,
+        fontFamily: "Arial",
     },
     subtitleText : {
-        fontFamily: "Coolvetica",
+        fontFamily: "Arial",
         fontSize: 20,
         textAlign: "left",
         letterSpacing: 1,
         color: Res.FTCColors.MellowApricot
     },
     titleText: {
-        fontFamily: "Coolvetica",
+        fontFamily: "Arial",
         fontSize: 35,
         fontWeight: "bold",
         textAlign: "left",
         letterSpacing: 2,
-        color: Res.FTCColors.MellowApricot
+        color: "#85ACB9"
     },
     titleShape: {
         justifyContent: 'center',
         padding: 30,
         paddingTop: 45,
         paddingBottom: 25,
-        backgroundColor: Res.FTCColors.BlueLightsBackground
+        backgroundColor: "#DCF0F7",
     },
 });
